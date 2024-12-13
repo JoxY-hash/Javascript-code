4441b9171123dc18ecef2bc093457e8b78f8bf60
JoxY-hash <nikuze2020@gmail.com>
2024-12-13 10:14:42 +0200

    ome

---
+++ variables.js
@@ -0,0 +1,68 @@
+// DATA TYPE
+let myName ="josian";
+myName ="kelly"
+
+// console.log(myName); 
+
+// let var const
+// + *
+let a = 12;
+let b = 10;
+
+// console.log(a+b);
+
+// CONDITIONS
+
+let age = 16;
+
+if(age < 18){
+    // console.log('you are allowed to drink')
+
+}else{
+    // console.log('you are not allowed to drink')
+
+}
+
+let BankAccount =60000;
+if(BankAccount >=1000000){
+    // console.log('you rich ')?
+}else if(BankAccount ==500000){
+    //  console.log('you good ')
+
+}else{
+    //  console.log('you broke')
+}
+let country = Rwanda
+if(country == Muraho){
+    // console.log('Muraho')
+
+}
+else if ('country == UK')
+if (country = Hello){
+    // console.log('Hello')
+
+}
+
+else if('country == france')
+if(country =Bjr){
+    // console.log('Bjr')
+
+}
+else if('country == tanzania')
+if(country = kwiswahili){
+    // console.log('Habari za subuhi')
+}
+
+let country = "Rwanda"
+if(country == Rwanda){
+    console.log('Muraho')
+}
+else{
+    console.log('Rwanda')
+    
+}
+
+
+
+
+
